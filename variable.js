fe6064dcae6f83906f648a2c2609a95bd452832b
Genius-Excel <geniusexcel9@gmail.com>
2023-12-11 10:39:16 +0100

    concatenating variable with a string.

--- variable.js
+++ variable.js
@@ -3,3 +3,9 @@ var my_name;
 my_name = "Genius Excel";
 
 console.log(my_name)
+
+let college_name;
+
+college_name = "Harvard University";
+
+console.log("My name is ", my_name, "I am a student of ", college_name);
