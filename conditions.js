ae9837b29b9175a6f360812341b6022932b5c701
Genius-Excel <geniusexcel9@gmail.com>
2023-12-11 10:53:45 +0100

    Conditionals in JS.

---
+++ conditions.js
@@ -0,0 +1,15 @@
+var day;
+
+var weather;
+
+weather = "Raining.";
+
+day = "Saturday";
+
+if (day == "Saturday"){
+    console.log("It is weekend, let's go out to play!. :)");
+}
+
+if (weather){
+    console.log("Oh no!, it's raining we can't go out to play anymore :(.")
+}
