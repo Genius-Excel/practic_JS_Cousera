4acf1faea1eea9f476ed2981fc43d65e0c8b4801
Genius-Excel <geniusexcel9@gmail.com>
2023-12-11 10:45:45 +0100

    I implemented a different method of string interpolation.

--- variable.js
+++ variable.js
@@ -9,3 +9,11 @@ let college_name;
 college_name = "Harvard University";
 
 console.log("My name is ", my_name, "I am a student of ", college_name);
+
+var hubby;
+
+hubby = "Programming";
+
+console.log("----concatenating varible another with another style----");
+
+console.log(`My name is ${my_name}, my hubby is ${hubby}`)
