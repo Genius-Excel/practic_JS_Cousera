fc26c512c82782ae2ebc3da84f9fc486c20a975d
Genius-Excel <geniusexcel9@gmail.com>
2023-12-11 10:57:58 +0100

    Else-if condition included.

--- conditions.js
+++ conditions.js
@@ -6,10 +6,16 @@ weather = "Raining.";
 
 day = "Saturday";
 
+day = "Monday";
+
 if (day == "Saturday"){
     console.log("It is weekend, let's go out to play!. :)");
+}
+else if (day == "Monday"){
+    console.log("It's time to work and be productive.")
 }
 
 if (weather){
     console.log("Oh no!, it's raining we can't go out to play anymore :(.")
 }
+
