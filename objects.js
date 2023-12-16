f75f88daec4d4f4ab66b85de7c04c9e0e94c0404
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 10:27:26 +0100

    OBjects in JS.

---
+++ objects.js
@@ -0,0 +1,20 @@
+var my_object = {
+    first_name: "Genius",
+    last_name: "Excel",
+    hobby: "Programming",
+    city: "Unknown",
+    country: "Unknown"
+};
+
+console.log(my_object);
+
+console.log(my_object.first_name)
+
+console.log("-------Changing Object last_name attribute-----");
+
+var new_name;
+new_name = "Favour";
+
+my_object.last_name = new_name;
+
+console.log(my_object.last_name);
