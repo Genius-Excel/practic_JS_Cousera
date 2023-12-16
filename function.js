dc70bc939394c3ece0f4c37426e706a8825d827f
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 09:32:06 +0100

    File modified to include subtraction function.

--- function.js
+++ function.js
@@ -15,3 +15,18 @@ result = additon(a, b);
 console.log(`The addition of ${a} and ${b} is ${result}`);
 
 
+function subtraction(num1, num2)
+{
+    return (num1 - num2);
+}
+
+var a = 100;
+var b = 50;
+
+var result;
+
+result = subtraction(a, b);
+
+console.log(`The subtraction of ${a} and ${b} is ${result}`);
+
+
