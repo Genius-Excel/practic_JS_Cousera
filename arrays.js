eb4b19f8bde331f7b6fcc9eeb68966ee9b7e3c59
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 10:11:00 +0100

    File updated.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-// This file contains the implementation of objects and arrays.
+// This file contains the implementation of elements and arrays.
 
 my_array = ["Orange", "Grapes", "Banana", "Mangoes"]
 
@@ -10,7 +10,7 @@ console.log(`Element at index 2 in my_arrays is ${my_array[2]}`);
 console.log(`Element at index 3 in my_array is ${my_array[3]}`);
 
 
-// Iterating through the objects of an array.
+// Iterating through the elements of an array.
 
 var i;
 
