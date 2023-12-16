7de26ec04c9601a747b0b79ea894b9ee56c74254
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 09:27:56 +0100

    This file contains a function that calculates the sum of two numbers.

---
+++ function.js
@@ -0,0 +1,17 @@
+// This is a function that returns the sum of two integers.
+
+function additon(num1, num2)
+{
+    return (num1 + num2);
+}
+
+var a = 20;
+var b = 50;
+
+var result;
+
+result = additon(a, b);
+
+console.log(`The addition of ${a} and ${b} is ${result}`);
+
+
