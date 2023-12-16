48b364cacbb6d46c40917914cb6b03630f0d3ffd
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 09:42:39 +0100

    This file contains the implementation of objects and arrays.

--- arrays.js
+++ arrays.js
@@ -2,6 +2,9 @@
 
 my_array = ["Orange", "Grapes", "Banana", "Mangoes"]
 
-console.log(my_array.length)
+console.log(my_array.length);
 
-console.log(`element at index 0 of my_array is ${my_array[0]}`)
+console.log(`element at index 0 of my_array is ${my_array[0]}`);
+console.log(`Element at index 1 in my_array is ${my_array[1]}`);
+console.log(`Element at index 2 in my_arrays is ${my_array[2]}`);
+console.log(`Element at index 3 in my_array is ${my_array[3]}`);
