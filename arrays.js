33afa1da1d785796957580b0169f626df62958d2
Genius-Excel <geniusexcel9@gmail.com>
2023-12-16 09:54:40 +0100

    Using a for loop to iterte every elements in an array.

--- arrays.js
+++ arrays.js
@@ -8,3 +8,21 @@ console.log(`element at index 0 of my_array is ${my_array[0]}`);
 console.log(`Element at index 1 in my_array is ${my_array[1]}`);
 console.log(`Element at index 2 in my_arrays is ${my_array[2]}`);
 console.log(`Element at index 3 in my_array is ${my_array[3]}`);
+
+
+// Iterating through the objects of an array.
+
+var i;
+
+var arr_len;
+
+arr_len = my_array.length;
+
+// This for loop will print all the elements present in my_array.
+
+for (i = 0; i < arr_len; i++)
+{
+    console.log(my_array[i]);
+}
+
+
