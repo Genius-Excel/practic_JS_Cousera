657122add34c5d37ae3ebac15a281965d58f8bc5
Genius-Excel <geniusexcel9@gmail.com>
2023-12-26 14:17:43 +0100

    OOP in JS.

---
+++ train.js
@@ -0,0 +1,10 @@
+// Object Oriented Programming in JavaScript.
+
+class Train{
+    constructor(colour, lights_on){
+        this.colour = colour;
+        this.lights_on = lights_on;
+    }
+}
+
+new Train("red", true);
