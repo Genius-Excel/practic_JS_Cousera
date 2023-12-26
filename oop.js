3c4736e7ebdeb8f3128a284af9accbcf53ab7574
Genius-Excel <geniusexcel9@gmail.com>
2023-12-26 13:37:48 +0100

    File modified.

--- oop.js
+++ oop.js
@@ -14,6 +14,21 @@ class Mammal{
     
 };
 
-var obj = new Mammal("Land", "Vegetables");
+//var obj = new Mammal("Land", "Vegetables");
 
-obj.about();
+//obj.about();
+
+class Dog extends Mammal{
+    about(){
+        super.about();
+    }
+
+    comm_sound(sound){
+        console.log(`While on ${this.habitat}, I make the ${sound} sound.`)
+    }
+}
+
+var new_obj = new Dog("Land", "Meat");
+
+new_obj.about();
+new_obj.comm_sound("Barking");
