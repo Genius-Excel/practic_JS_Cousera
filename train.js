fa30ef13c1084fb8ba0b6b04ba7e5af4dd684f77
Genius-Excel <geniusexcel9@gmail.com>
2023-12-26 14:29:44 +0100

    File modified.

--- train.js
+++ train.js
@@ -5,8 +5,26 @@ class Train{
         this.colour = colour;
         this.lights_on = lights_on;
     }
+    
+    // This method toggles the light.
+    toggle_light(){
+        this.lights_on = !this.lights_on;
+    }
+
+    light_status(){
+        console.log(`Lights on?`, this.lights_on);
+    }
+
+    get_self(){
+        console.log(this);
+    }
+
+    get_prototype(){
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
 }
 
 var my_first_trian = new Train("red", true);
 
-console.log(my_first_trian);
+my_first_trian.light_status();
