73d509fb4a5b3c18b9ee59d337315d85efb41393
Genius Excel <124288507+Genius-Excel@users.noreply.github.com>
2024-01-07 22:35:17 +0100

    Update text1.js

--- text1.js
+++ text1.js
@@ -1,5 +1,4 @@
-// this file is a simple loop to show that
-// this loop shows that a string is also an array
+// this file is a simple loop shows that a string is also an array
 var i;
 var string_len;
 
