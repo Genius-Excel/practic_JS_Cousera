1ff5f0aa018d81f0f77e6f004de502215f2926b8
Genius Excel <124288507+Genius-Excel@users.noreply.github.com>
2024-01-22 22:13:14 +0100

    File updated

--- calc.js
+++ calc.js
@@ -1,10 +1,12 @@
 // This file will contain an implementation of a simple calculator program.
 console.log("Enter numbers");
+
 var num1 = 10;
 var num2 = 20;
 console.log("The sum of num1 and num2 is  ");
 var result = num1 + num2;
 
 console.log (result);
+// answer --> 30
 // this is a simple calculation in Js.
 // code documentation
